5814907baa261d88275b2d2d879b186fe3b7dba2
trekroot <trekroot@gmail.com>
2017-03-20 23:08:02 -0400

    Submission #3: added blanks, cleaned up functions and variables

--- stage_2/project/fill-in-the-blanks.py
+++ stage_2/project/fill-in-the-blanks.py
@@ -16,17 +16,11 @@
 # To help you get started, we've provided a sample paragraph that you can use when testing your code.
 # Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
 
-easy = '''A common first thing to do in a language is display
-'Hello __1__!'  In __2__ this is particularly easy; all you have to do
-is type in: __3__ "Hello __1__!" Of course, that isn't a very useful thing to do.
-However, it is an example of how to output to the user using the __3__ command, and
-produces a program which does something, so it is useful in that capacity.'''
-
-samples = ['''A common first thing to do in a language is display
-'Hello __1__!'  In __2__ this is particularly easy; all you have to do
-is type in: __3__ "Hello __1__!" Of course, that isn't a very useful thing to do.
-However, it is an example of how to output to the user using the __3__ command, and
-produces a program which does something, so it is useful in that capacity.''',
+samples = ['''A __1__ is created with the def keyword.  You specify the inputs a
+__1__ takes by adding __2__ separated by commas between the parentheses.
+__1__s by default returns __3__ if you don't specify the value to retrun.
+__2__ can be standard data types such as string, integer, dictionary, tuple,
+and __4__ or can be more complicated such as objects and lambda functions.''',
 
 '''A common first thing to do in a language is display
 'Hello __1__!'  In __2__ this is particularly easy; all you have to do
@@ -37,16 +31,21 @@ It may seem a bit odd to do something in a Turing complete language that
 can be done even more easily with an __4__ file in a browser, but it's
 a step in learning __2__ syntax, and that's really its purpose.''',
 
-'''A __1__ is created with the def keyword.  You specify the inputs a
-__1__ takes by adding __2__ separated by commas between the parentheses.
-__1__s by default returns __3__ if you don't specify the value to retrun.
-__2__ can be standard data types such as string, integer, dictionary, tuple,
-and __4__ or can be more complicated such as objects and lambda functions.''']
+'''When you create a __1__, certain __2__es are automatically
+generated for you if you don't make them manually. These contain multiple
+underscores before and after the word defining them.  When you write
+a __1__, you almost always include at least the __3__ __2__, defining
+variables for when __4__s of the __1__ get made.  Additionally, you generally
+want to create a __5__ __2__, which will allow a string representation
+of the method to be viewed by other developers.''']
+
 
 
 # The answer for ___1___ is 'function'. Can you figure out the others?
-blanks = ['__1__','__2__','__3__','__4__']
-answers = [['world','python','print'],['world','python','print','HTML'],['function','arguments','none','boolean']]
+blanks = ['__1__','__2__','__3__','__4__','__5__']
+answers = [ ['function','arguments','none','boolean'],
+            ['world','python','print','HTML'],
+            ['function','class','public','private','protected']]
 levels = ['easy','medium','hard']
 
 def catch_non_int(value):
@@ -56,57 +55,60 @@ def catch_non_int(value):
     except ValueError as e:
         print 'That is not an integer!'
 
+#prompts for number of guesses and validates type int (no tricks!)
 def level_num_guesses():
-    number = raw_input("Please enter an number of guesses you want per missing word (3 is suggested number): ")
-    val = catch_non_int(number)
+    max_guesses = raw_input("Please enter an number of guesses you want per missing word (3 is suggested number): ")
+    val = catch_non_int(max_guesses)
     while type(val) != int:
-        number = raw_input("Please enter an integer for number of guesses you want per missing word (3 is suggested number): ")
-        val = catch_non_int(number)
-    return number
+        max_guesses = raw_input("Please enter an integer for number of guesses you want per missing word (3 is suggested number): ")
+        val = catch_non_int(max_guesses)
+    return max_guesses
 
+#prompts and sets game difficulty
 def text_difficulty():
     level = raw_input("Please enter difficulty of the text you must fill: " + levels[0] + ", " + levels[1] + ", or " + levels[2] + ": ").lower()
     while level not in levels:
-        level = raw_input("Please enter a correct difficulty: " + levels[0] + ", " + levels[1] + ", or " + levels[2] + ": ")
-    game_text = samples[levels.index(str(level))]
+        level = raw_input("Please enter a correct difficulty: " + levels[0] + ", " + levels[1] + ", or " + levels[2] + ": ").lower()
+    game_text = samples[levels.index(level)]
     return game_text
 
-def word_missing(word,attempts,level_index):
-    user_tries = 0
-    while user_tries < attempts: #tries loop
-        user_input = raw_input("Enter word for " + word + ": ") #require input
+#if a word is missing, word_missing checks that the correct word is input within attempt limit
+def word_missing(word,max_attempts,level_index):
+    user_attempt = 0
+    while user_attempt < max_attempts: #tries loop
+        user_input = raw_input("Enter word for " + word + ": ") #prompt input
+        user_attempt += 1
         if user_input.lower() == answers[level_index][blanks.index(word)]: #validate input is correct
             print("\n Correct! \n")
             return user_input.lower()
-        user_tries += 1
-
-        if user_tries == attempts: #if incorrect
+        elif user_attempt == int(max_attempts): #if incorrect
             print("\n \n \n Game over. Please play again.")
             exit() #user loses the game, exit script
-        print("\n Try Again. You have " + str(int(attempts) - user_tries) + " more attempt(s) \n")
+        print("\n Try Again. You have " + str(int(max_attempts) - user_attempt) + " more attempt(s) \n")
     return word
 
 def check_word(word,blanks): #function to check if word-part is in blanks
-    for e in blanks:
-        if e in word:
+    for blank in blanks:
+        if blank in word:
             return True
     return False
 
-def trim_word(word,blanks): #function to check if word-part is in blanks
-    for e in blanks:
-        if e in word:
-            return e
+def trim_word(word,blanks): #function to trim word-part
+    for blank in blanks:
+        if blank in word:
+            return blank
     return False
 
-def fill_in_the_blanks(text,blanks,attempts):
+#utilize functions to make game playable
+def user_prompts_validation(text,blanks,attempts):
     text_list = text.split()
-    level_number = samples.index(text)
-    for e in text_list:
-        if e in text_list and check_word(e, blanks): #is e in updated text_list
-            e_trim = trim_word(e,blanks)
+    level_number = samples.index(text) #get level number for validation
+    for word in text_list: #go throught text by each word
+        if word in text_list and check_word(word, blanks): #is e in text_list
+            trimmed_word = trim_word(word,blanks) #prep word part
             print text
-            next_word = word_missing(e_trim,attempts,level_number)
-            text = text.replace(e_trim,next_word) #replace ALL occurrences
+            next_word = word_missing(trimmed_word,attempts,level_number) #primary 'game' function
+            text = text.replace(trimmed_word,next_word) #replace ALL occurrences
             text_list = text.split()
     print(text)
     print("\n \n ************* \n Congratulations, you won! \n *************")
@@ -119,12 +121,9 @@ def play_game():
     print "\n \n Welcome to Fill In The Blanks! Follow the prompts to enter your difficulty settings. \n \n"
     attempts_total = level_num_guesses()
     sample = text_difficulty()
-    fill_in_the_blanks(sample,blanks,attempts_total)
+    user_prompts_validation(sample,blanks,attempts_total)
     return attempts_total
 
-
-# check_word('___1___',blanks)
-# fill_in_the_blanks(easy,blanks,3)
 play_game()
 
 # We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
